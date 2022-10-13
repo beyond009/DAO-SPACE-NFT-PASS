6e727e5535ff71f08326632631ecd4b96a205c62
byeond009 <1249413181@qq.com>
2022-10-13 14:21:58 +0800

    update

--- src/components/Common/Modal/index.tsx
+++ src/components/Common/Modal/index.tsx
@@ -1,13 +1,10 @@
 import * as React from "react";
-import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 import Slide from "@mui/material/Slide";
 import { TransitionProps } from "@mui/material/transitions";
-
+import styled from "styled-components";
+import { Gap } from "..";
 const Transition = React.forwardRef(function Transition(
   props: TransitionProps & {
     children: React.ReactElement<any, any>;
@@ -17,6 +14,67 @@ const Transition = React.forwardRef(function Transition(
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
+const TitleFont = styled.div`
+  font-family: Epilogue;
+  font-size: 24px;
+  line-height: 36px;
+  color: #171a1fff;
+  font-weight: 800;
+`;
+const ValueFont = styled.div`
+  font-family: Inter;
+  font-size: 14px;
+  line-height: 22px;
+  color: #171a1fff;
+`;
+const Button = styled.div`
+  width: 120px;
+  height: 36px;
+  padding: 0 12px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: Inter;
+  font-size: 14px;
+  line-height: 22px;
+  color: #ffffffff;
+  background: #7d6ce2ff;
+  opacity: 1;
+  border: none;
+  border-radius: 4px;
+  :hover {
+    color: #ffffffff;
+    background: #644fdcff;
+  }
+  :active {
+    color: #ffffffff;
+    background: #4d35d7ff;
+  }
+`;
+const GrayButton = styled.div`
+  width: 120px;
+  height: 36px;
+  padding: 0 12px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: Inter;
+  font-size: 14px;
+  line-height: 22px;
+  color: #565e6cff;
+  background: #f3f4f6ff;
+  opacity: 1;
+  border: none;
+  border-radius: 4px;
+  :hover {
+    color: #565e6cff;
+    background: #dee1e6ff;
+  }
+  :active {
+    color: #565e6cff;
+    background: #cfd2daff;
+  }
+`;
 interface Props {
   visible: boolean;
   setVisible: Fuction;
@@ -28,16 +86,22 @@ export const Modal = ({ visible, setVisible }: Props) => {
       TransitionComponent={Transition}
       keepMounted
       onClose={() => setVisible(false)}
-      aria-describedby="alert-dialog-slide-description"
     >
-      <DialogTitle>{"Use Google's location service?"}</DialogTitle>
-      <DialogContent>
-        <DialogContentText id="alert-dialog-slide-description">
-          Let Google help apps determine location. This means sending anonymous
-          location data to Google, even when no apps are running.
-        </DialogContentText>
-      </DialogContent>
-      <DialogActions></DialogActions>
+      <div className="flex flex-col items-center" style={{ width: "300px" }}>
+        <Gap height={30} />
+        <TitleFont>开门成功</TitleFont>
+        <Gap height={6} />
+        <ValueFont>感谢使用DAO Space NFT Pass</ValueFont>
+        <ValueFont>您为66解锁了第72个假期 🚀</ValueFont>
+        <ValueFont>祝你拥有美好的一天 👾</ValueFont>
+        <Gap height={28} />
+        <div className="flex">
+          <GrayButton onClick={() => setVisible(false)}>打不开？</GrayButton>
+          <Gap width={16} />
+          <Button onClick={() => setVisible(false)}>确认</Button>
+        </div>
+        <Gap height={24} />
+      </div>
     </Dialog>
   );
 };
