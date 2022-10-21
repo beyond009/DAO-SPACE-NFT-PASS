b08f23dca727f9492b2456f713c1af1037443ab1
byeond009 <1249413181@qq.com>
2022-10-21 16:06:14 +0800

    fix

--- src/assets/api/proxy.js
+++ src/assets/api/proxy.js
@@ -2,7 +2,7 @@
  * @Author: byeond009 1249413181@qq.com
  * @Date: 2022-10-21 16:02:01
  * @LastEditors: byeond009 1249413181@qq.com
- * @LastEditTime: 2022-10-21 16:03:33
+ * @LastEditTime: 2022-10-21 16:06:03
  * @FilePath: /vite-react-ts/src/assets/api/proxy.js
  * @Description:
  *
@@ -13,7 +13,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = (req, res) => {
   let target = "";
 
-  if (req.url.startsWith("/api2")) {
+  if (req.url.startsWith("/api")) {
     target = "https://61618mh025.zicp.fun" + req.url.replace(/^\/api/, "");
     createProxyMiddleware({
       target,
